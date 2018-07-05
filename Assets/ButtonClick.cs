15f97a168602bc8b307bf9b67609ec69f4e095ac
DESKTOP-NCQTDBE\Malte <malteweiss93@gmail.com>
2018-07-05 23:07:26 +0200

    Added demo scene

---
+++ Assets/ButtonClick.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonClick : MonoBehaviour {
+
+    [SerializeField]
+    private ControlDoor door;
+
+    public Animator animator;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnMouseDown()
+    {
+        Debug.Log("lol");
+        var result = door.ToggleDoor();
+        animator.SetBool("active", result);
+    }
+}
---
+++ Assets/ControlDoor.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControlDoor : MonoBehaviour {
+
+    private bool isOpened = false;
+    public Animator animator;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public bool ToggleDoor()
+    {
+        isOpened = !isOpened;
+        animator.SetBool("active", isOpened);
+        return isOpened;
+    }
+}
